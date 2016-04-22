c97e3af3bf9547b32e98703e123f488850556913
ttru <truongtommy06@gmail.com>
2016-04-22 15:56:34 -0700

    Began work on controller logic

---
+++ TTTController.js
@@ -0,0 +1,24 @@
+// Displays a message to the player.
+var displayMessage = function(message) {
+
+}
+
+// Clears the board of marks.
+var clearBoard = function() {
+
+}
+
+// Hooks up the game graphics and logic for the given game mode:
+// - "PC" : player goes first (as X) vs. computer
+// - "CP" : player goes second (as O) vs. computer
+// - "PP" : player 1 vs player 2
+var createGame = function(gameMode) {
+  clearBoard();
+  var boardModel = new TTTModel();
+}
+
+// Initial setup of game and UI elements.
+var setUp = function() {
+  //
+
+}
