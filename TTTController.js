924c63cfc84e2c17c668faa76fcd96dd3b08edd9
ttru <truongtommy06@gmail.com>
2016-05-24 11:09:21 -0700

    Filled out more controller logic

--- TTTController.js
+++ TTTController.js
@@ -1,11 +1,13 @@
 // Displays a message to the player.
 var displayMessage = function(message) {
-
+  document.getElementById("message").innerHTML = message;
 }
 
 // Clears the board of marks.
 var clearBoard = function() {
-
+  for (var i = 0; i < 9; i++) {
+    document.getElementById('' + i).innerHTML = " ";
+  }
 }
 
 // Hooks up the game graphics and logic for the given game mode:
@@ -15,10 +17,16 @@ var clearBoard = function() {
 var createGame = function(gameMode) {
   clearBoard();
   var boardModel = new TTTModel();
+  // TODO: Set up event listeners for each square in table.
+  // Add logic to determine wins and display appropriate messages.
 }
 
 // Initial setup of game and UI elements.
 var setUp = function() {
-  //
-
+  document.getElementById("new-game-button").onclick = function() {
+    var modeSelect = document.getElementById("mode-select");
+    createGame(modeSelect.options[modeSelect.selectedIndex].value);
+  };
 }
+
+window.onload = setUp;
